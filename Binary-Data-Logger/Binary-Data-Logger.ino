8832e17f496e9150c3fd821e56997fde92d36691
Alex <alexschell@protonmail.com>
2016-05-18 18:16:32 -0700

    Add Binary Logger and Csv Converter
    Finally.

---
+++ Binary-Data-Logger/Binary-Data-Logger.ino
@@ -0,0 +1,349 @@
+/* Adaptaion of Bill Greiman's LowLatancyLogger SdFat example to make use of the LSM9DS0 library
+   Uses RAM buffer and SDFat's buffer to write data in 512 byte blocks, allowing higher writing speed
+   Will write to a binary file, and as such must be converted using the binaryDecoder found in computer-side coverter
+   If you need a Simpler logger, use the basic data-logger.
+*/
+#include <SPI.h>
+#include <SdFat.h>
+#include <SdFatUtil.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM9DS0.h>
+//addresses and values for writing to the LSM9DS0
+typedef enum {
+  LSM9DS0_REGISTER_OUT_X_L_G           = 0x28,
+  LSM9DS0_REGISTER_CTRL_REG4_G         = 0x23,
+} lsm9ds0GyroRegisters_t;
+typedef enum {
+  LSM9DS0_REGISTER_TEMP_OUT_L_XM       = 0x05,
+  LSM9DS0_REGISTER_OUT_X_L_M           = 0x08,
+  LSM9DS0_REGISTER_CTRL_REG1_XM        = 0x20,
+  LSM9DS0_REGISTER_OUT_X_L_A           = 0x28,
+  LSM9DS0_REGISTER_CTRL_REG6_XM        = 0x25,
+}lsm9ds0MagAccelRegisters_t;
+typedef enum {
+  LSM9DS0_ACCELDATARATE_1600HZ         = (0b1010 << 4)
+}lm9ds0AccelDataRate_t;
+typedef enum {
+  LSM9DS0_MAGDATARATE_100HZ            = (0b101 << 2)
+} lsm9ds0MagDataRate_t;
+typedef enum {
+  LSM9DS0_GYROSCALE_2000DPS            = (0b10 << 4)
+} lsm9ds0GyroScale_t;
+
+
+#include "UserDataType.h"
+Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0();
+//----------------------------------------------
+void acquireData(data_t* data) {
+    data->time = micros();
+    lsm.readAccel();
+    data->adc[0] = lsm.accelData.x;
+    data->adc[1] = lsm.accelData.y;
+    data->adc[2] = lsm.accelData.z;
+    lsm.readGyro();
+    data->adc[3] = lsm.gyroData.x;
+    data->adc[4] = lsm.gyroData.y;
+    data->adc[5] = lsm.gyroData.z;
+    /*
+    lsm.readMag();
+    data->adc[6] = lsm.magData.x;
+    data->adc[7] = lsm.magData.y;
+    data->adc[8] = lsm.magData.z;
+    */
+}//acquireData
+//-------------------------------------------
+//Interval between data records in microseconds.
+const uint32_t LOG_INTERVAL_USEC = 3500;
+// SD chip select pin.
+const uint8_t SD_CS_PIN = SS;
+
+// Maximum file size in blocks.
+// The program creates a contiguous file with FILE_BLOCK_COUNT 512 byte blocks.
+// This file is flash erased using special SD commands.  The file will be
+// truncated if logging is stopped early.
+const uint32_t FILE_BLOCK_COUNT = 256000;
+
+// log file base name.  Must be six characters or less.
+#define FILE_BASE_NAME "data"
+
+//select number of buffer blocks based on end address of ram uno has 1
+
+const uint8_t BUFFER_BLOCK_COUNT = 1;
+#define TMP_FILE_NAME "tmp_log.bin"
+
+// Size of file base name.  Must not be larger than six.
+const uint8_t BASE_NAME_SIZE = sizeof(FILE_BASE_NAME) - 1;
+
+SdFat sd;
+
+SdBaseFile binFile;
+
+char binName[13] = FILE_BASE_NAME "00.bin";
+
+// Number of data records in a block.
+const uint16_t DATA_DIM = (512 - 4)/sizeof(data_t);
+
+//Compute fill so block size is 512 bytes.  FILL_DIM may be zero.
+const uint16_t FILL_DIM = 512 - 4 - DATA_DIM*sizeof(data_t);
+
+struct block_t {
+  uint16_t count;
+  uint16_t overrun;
+  data_t data[DATA_DIM];
+  uint8_t fill[FILL_DIM];
+};
+
+const uint8_t QUEUE_DIM = BUFFER_BLOCK_COUNT + 2;
+
+block_t* emptyQueue[QUEUE_DIM];
+uint8_t emptyHead;
+uint8_t emptyTail;
+
+block_t* fullQueue[QUEUE_DIM];
+uint8_t fullHead;
+uint8_t fullTail;
+
+// Advance queue index.
+inline uint8_t queueNext(uint8_t ht) {
+  return ht < (QUEUE_DIM - 1) ? ht + 1 : 0;
+}
+// Error messages stored in flash.
+#define error(msg) errorFlash(F(msg))
+//------------------------------------------------------------------------------
+void errorFlash(String msg) {
+  Serial.println(msg);
+}
+//--------------------------------------------------------------------------
+// log data
+uint32_t const ERASE_SIZE = 262144L;
+void logData() {
+  uint32_t bgnBlock, endBlock;
+
+  // Allocate extra buffer space.
+  block_t block[BUFFER_BLOCK_COUNT];
+  block_t* curBlock = 0;
+  Serial.println();
+
+  // Find unused file name.
+  if (BASE_NAME_SIZE > 6) {
+    error("FILE_BASE_NAME too long");
+  }
+  while (sd.exists(binName)) {
+    if (binName[BASE_NAME_SIZE + 1] != '9') {
+      binName[BASE_NAME_SIZE + 1]++;
+    } else {
+      binName[BASE_NAME_SIZE + 1] = '0';
+      if (binName[BASE_NAME_SIZE] == '9') {
+        error("Can't create file name");
+      }
+      binName[BASE_NAME_SIZE]++;
+    }
+  }
+  // Delete old tmp file.
+  if (sd.exists(TMP_FILE_NAME)) {
+    Serial.println(F("Deleting tmp file"));
+    if (!sd.remove(TMP_FILE_NAME)) {
+      error("Can't remove tmp file");
+    }
+  }
+  // Create new file.
+  Serial.println(F("Creating new file"));
+  binFile.close();
+  if (!binFile.createContiguous(sd.vwd(),
+                                TMP_FILE_NAME, 512 * FILE_BLOCK_COUNT)) {
+    error("createContiguous failed");
+  }
+  // Get the address of the file on the SD.
+  if (!binFile.contiguousRange(&bgnBlock, &endBlock)) {
+    error("contiguousRange failed");
+  }
+  // Use SdFat's internal buffer.
+  uint8_t* cache = (uint8_t*)sd.vol()->cacheClear();
+  if (cache == 0) {
+    error("cacheClear failed");
+  }
+
+  // Flash erase all data in the file.
+  Serial.println(F("Erasing all data"));
+  uint32_t bgnErase = bgnBlock;
+  uint32_t endErase;
+  while (bgnErase < endBlock) {
+    endErase = bgnErase + ERASE_SIZE;
+    if (endErase > endBlock) {
+      endErase = endBlock;
+    }
+    if (!sd.card()->erase(bgnErase, endErase)) {
+      error("erase failed");
+    }
+    bgnErase = endErase + 1;
+  }
+  // Start a multiple block write.
+  if (!sd.card()->writeStart(bgnBlock, FILE_BLOCK_COUNT)) {
+    error("writeBegin failed");
+  }
+  // Initialize queues.
+  emptyHead = emptyTail = 0;
+  fullHead = fullTail = 0;
+
+  // Use SdFat buffer for one block.
+  emptyQueue[emptyHead] = (block_t*)cache;
+  emptyHead = queueNext(emptyHead);
+
+  // Put rest of buffers in the empty queue.
+  for (uint8_t i = 0; i < BUFFER_BLOCK_COUNT; i++) {
+    emptyQueue[emptyHead] = &block[i];
+    emptyHead = queueNext(emptyHead);
+  }
+  Serial.println(F("Logging - type any character to stop"));
+  // Wait for Serial Idle.
+  Serial.flush();
+  delay(10);
+  uint32_t bn = 0;
+  uint32_t t0 = millis();
+  uint32_t t1 = t0;
+  uint32_t overrun = 0;
+  uint32_t overrunTotal = 0;
+  uint32_t count = 0;
+  uint32_t maxLatency = 0;
+  int32_t diff;
+  // Start at a multiple of interval.
+  uint32_t logTime = micros()/LOG_INTERVAL_USEC + 1;
+  logTime *= LOG_INTERVAL_USEC;
+  bool closeFile = false;
+  while (1) {
+    // Time for next data record.
+    logTime += LOG_INTERVAL_USEC;
+    if (Serial.available()) {
+      closeFile = true;
+    }
+
+    if (closeFile) {
+      if (curBlock != 0 && curBlock->count >= 0) {
+        // Put buffer in full queue.
+        fullQueue[fullHead] = curBlock;
+        fullHead = queueNext(fullHead);
+        curBlock = 0;
+      }
+    } else {
+      if (curBlock == 0 && emptyTail != emptyHead) {
+        curBlock = emptyQueue[emptyTail];
+        emptyTail = queueNext(emptyTail);
+        curBlock->count = 0;
+        curBlock->overrun = overrun;
+        overrun = 0;
+      }
+      do {
+        diff = logTime - micros();
+      } while(diff > 0);
+      if (diff < -10) {
+        error("LOG_INTERVAL_USEC too small");
+      }
+      if (curBlock == 0) {
+        overrun++;
+      } else {
+        acquireData(&curBlock->data[curBlock->count++]);
+        //if we reach max number of logs per buffer block move in queue
+        if (curBlock->count == DATA_DIM) {
+          fullQueue[fullHead] = curBlock;
+          fullHead = queueNext(fullHead);
+          curBlock = 0;
+        }
+      }
+    }
+
+    if (fullHead == fullTail) {
+      // Exit loop if done.
+      if (closeFile) {
+        break;
+      }
+    } else if (!sd.card()->isBusy()) {
+      // Get address of block to write.
+      block_t* pBlock = fullQueue[fullTail];
+      fullTail = queueNext(fullTail);
+      // Write block to SD.
+      uint32_t usec = micros();
+      if (!sd.card()->writeData((uint8_t*)pBlock)) {
+        error("write data failed");
+      }
+      usec = micros() - usec;
+      t1 = millis();
+      if (usec > maxLatency) {
+        maxLatency = usec;
+      }
+      count += pBlock->count;
+
+      // Add overruns and possibly light LED.
+      if (pBlock->overrun) {
+        overrunTotal += pBlock->overrun;
+      }
+      // Move block to empty queue.
+      emptyQueue[emptyHead] = pBlock;
+      emptyHead = queueNext(emptyHead);
+      bn++;
+      if (bn == FILE_BLOCK_COUNT) {
+        // File full so stop
+        break;
+      }
+    }
+  }
+  if (!sd.card()->writeStop()) {
+    error("writeStop failed");
+  }
+  // Truncate file if recording stopped early.
+  if (bn != FILE_BLOCK_COUNT) {
+    Serial.println(F("Truncating file"));
+    if (!binFile.truncate(512L * bn)) {
+      Serial.println(F("Catch truncate error"));
+    }
+  }
+  if (!binFile.rename(sd.vwd(), binName)) {
+    Serial.println(F("Catch rename error"));
+  }
+  Serial.print(F("Samples/sec: "));
+  Serial.println((1000.0)*count/(t1-t0));
+  Serial.println(F("Done"));
+}//logData
+//------------------------------------------------------------------------------
+
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial) {}
+  //setup sensors and write to set sensetivity
+  lsm.begin();
+  //accelation
+  lsm.write8(XMTYPE, LSM9DS0_REGISTER_CTRL_REG1_XM, LSM9DS0_ACCELDATARATE_1600HZ);
+  //magnometer
+  //lsm.write8(XMTYPE, LSM9DS0_REGISTER_CTRL_REG6_XM, LSM9DS0_MAGDATARATE_100HZ);
+  //gyro
+  lsm.write8(XMTYPE, LSM9DS0_REGISTER_CTRL_REG4_G, LSM9DS0_GYROSCALE_2000DPS);
+  Serial.print(F("Records/block: "));
+  Serial.println(DATA_DIM);
+  if (sizeof(block_t) != 512) {
+    error("Invalid block size");
+  }
+  // initialize file system.
+  if (!sd.begin(SD_CS_PIN, SPI_FULL_SPEED)) {
+    sd.initErrorPrint();
+  }
+}//setup
+
+void loop(void) {
+  // discard any input
+  while (Serial.read() >= 0) {}
+  Serial.println();
+  Serial.println(F("type:"));
+  Serial.println(F("r - record data"));
+
+  while(!Serial.available()) {}
+  char c = tolower(Serial.read());
+
+  // Discard extra Serial data.
+  do {
+    delay(10);
+  } while (Serial.read() >= 0);
+   if (c == 'r') {
+    logData();
+  } else {
+    Serial.println(F("Invalid entry"));
+  }
+}//loop
---
+++ Computer-Side Converter/binaryDecoder.cpp
@@ -0,0 +1,90 @@
+/* Uses the LSMDS0 libary to retrive and convert binary files created by binary-data-logger.ino
+  Due to the nature of binary files, you will have to match the structs here to the ones you use in the logger
+  By using an offloaded converter, we can save space on the arduino and save time in retriving data.
+*/
+#include <algorithm>
+#include <cstdint>
+#include <fstream>
+#include <iterator>
+#include <string>
+#define BASE_NAME "data01";
+//Values adapted from LSM9DS0 library to use in converting raw data to proper types
+//----------------------------------------------------
+#define SENSORS_GRAVITY_STANDARD       (9.80665F)
+#define LSM9DS0_ACCEL_MG_LSB_2G        (0.061F)
+#define LSM9DS0_ACCEL_MG_LSB_4G        (0.122F)
+#define LSM9DS0_ACCEL_MG_LSB_6G        (0.183F)
+#define LSM9DS0_ACCEL_MG_LSB_8G        (0.244F)
+#define LSM9DS0_ACCEL_MG_LSB_16G       (0.732F)
+#define LSM9DS0_GYRO_DPS_DIGIT_245DPS  (0.00875F)
+#define LSM9DS0_GYRO_DPS_DIGIT_500DPS  (0.01750F)
+#define LSM9DS0_GYRO_DPS_DIGIT_2000DPS (0.07000F)
+//----------------------------------------------------
+//Set to the same value which you grab
+const float accel_mg_lsb = LSM9DS0_ACCEL_MG_LSB_2G;
+const float gyro_dps_digit = LSM9DS0_GYRO_DPS_DIGIT_245DPS;
+//create a data holder object and store in memory as a continous item
+#pragma pack(1)
+struct data_t {
+  std::uint32_t ms;
+  std::int16_t accelX;
+  std::int16_t accelY;
+  std::int16_t accelZ;
+  std::int16_t gyroX;
+  std::int16_t gyroY;
+  std::int16_t gyroZ;
+};
+
+const int BLOCK_SIZE = 512;
+const int DATA_DIM = (BLOCK_SIZE - 4) / sizeof(data_t);
+const int FILL_DIM = (BLOCK_SIZE - 4) - sizeof(data_t) * DATA_DIM;
+//create a block object and store in memory as a continous item
+#pragma pack(1)
+struct block_t {
+  std::uint16_t count;
+  std::uint16_t overrun;
+  data_t data[DATA_DIM];
+  std::uint8_t fill[FILL_DIM];
+};
+
+//for retriving info from block
+std::istream& operator>>(std::istream& stream, const data_t& data) {
+  stream.read((char*)&data, sizeof(data_t));
+  return stream;
+}
+
+//for retiving block data from the binary file
+std::istream& operator>>(std::istream& stream, const block_t& block) {
+  stream.read((char*)&block, sizeof(block_t));
+  return stream;
+}
+
+//for outputting data holder types, we want to have a standard format when we pass it, as well as convert the values in the data struct
+std::ostream& operator<<(std::ostream& stream, const data_t& data) {
+  stream << data.ms << ",";
+  stream << data.accelX * accel_mg_lsb * SENSORS_GRAVITY_STANDARD / 1000 << ",";
+  stream << data.accelY * accel_mg_lsb * SENSORS_GRAVITY_STANDARD / 1000 << ",";
+  stream << data.accelZ * accel_mg_lsb * SENSORS_GRAVITY_STANDARD / 1000 << ",";
+  stream << data.gyroX * gyro_dps_digit << ",";
+  stream << data.gyroY * gyro_dps_digit << ",";
+  stream << data.gyroZ * gyro_dps_digit;
+  return stream;
+}
+
+//for printing block types we wish to only pass the data struct
+std::ostream& operator<<(std::ostream& stream, const block_t& block) {
+  for (const data_t* data = block.data; data < block.data + block.count; data++)
+    stream << *data << "\n";
+  return stream;
+}
+
+int main() {
+  //prep files for write/read
+  std::ifstream bin("data01.bin", std::ifstream::binary);
+  std::ofstream csv("data.csv");
+  //preform the transfer using the above
+  std::copy(
+    std::istream_iterator<block_t>(bin),
+    std::istream_iterator<block_t>(),
+    std::ostream_iterator<block_t>(csv));
+}
