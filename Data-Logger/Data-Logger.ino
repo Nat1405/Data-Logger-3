b7e4c711d1dc0a1a60a8ea5ac9620ba6eca5f651
Alex <alex.schell01@protonmail.com>
2016-06-06 01:04:02 -0700

    Update fallback data logger
    Includes temperature sensing, time and trigger based starts, and status
    LEDs

--- Data-Logger/Data-Logger.ino
+++ Data-Logger/Data-Logger.ino
@@ -1,3 +1,7 @@
+/*A simpler, fallback data-logger that uses more recognizable methods to write to SD
+However, it still uses the SDfat libary in certain ways to improve logging
+consider using this if you want a simpler logger with less fiddling, for testing
+and if the binary logger breaks in some way */
 #include <SPI.h>
 #include <Time.h>
 #include <SdFat.h>
@@ -5,31 +9,60 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM9DS0.h>
-SdFat sd;
-SdFile logFile;
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Adafruit_NeoPixel.h>
+
+
+//---------------------------------User Options---------------------------------
+//Pin which the light data reader is on
+const int LIGHT_PIN = 2;
+//Pin which the funtion button is connected
+const int BUTTON_PIN = 0;
+//Data pin for temperature sensor
+const int ONE_WIRE_BUS = 2;
+//Pin for status light
+const int ERROR_LED_PIN = 7;
 //IMPORTANT: This value is the sample rate, and must be based on the SD card quality and it's write time
-const int SAMPLE_MS = 5;
+const int SAMPLE_MS = 100;
 // 1 if the setup is running on a breadboard
 const boolean ON_BREADBOARD =1;
-// make 0 if you don't want a header of column titles included
+// Set to 0 if you don't want a header of column titles included
 #define DATAHEADER 1
-#define START_ON_LAUNCH 1
+//Set to 1 if you want launch detection, otherwise will use delay below
+#define START_ON_LAUNCH 0
+//delay in minutes to wait before starting
+const int TIME_DELAY = 1;
+//------------------------------------------------------------------------------
 
+//convert to millsecs
+long MSTime = TIME_DELAY*1000;
+SdFat sd;
+SdFile logFile;
 Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0(1000);  // Use I2C, ID #1000
 #define LSM9DS0_XM_CS 10
 #define LSM9DS0_GYRO_CS 9
 #define LSM9DS0_SCLK 13
 #define LSM9DS0_MISO 12
 #define LSM9DS0_MOSI 11
+//setup for temperature sensors
+int resolution =9;
+unsigned long lastTempRequest = 0;
+int  delayInMillis = 0;
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature tempSensor(&oneWire);
+DeviceAddress tempDeviceAddress;
+
+int buttonState = 0;
 
 uint32_t logTime;
 void setupSensors() {
   // accelerometer range
-  lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_2G);
+  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_2G);
   //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_4G);
   //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_6G);
   //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_8G);
-  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_16G);
+  lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_16G);
 
   //magnetometer sensitivity
   lsm.setupMag(lsm.LSM9DS0_MAGGAIN_2GAUSS);
@@ -38,17 +71,37 @@ void setupSensors() {
   //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_12GAUSS);
 
   //gyroscope
-  lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_245DPS);
+  //lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_245DPS);
   //lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_500DPS);
-  //lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_2000DPS);
+  lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_2000DPS);
+  //pull up our first temp reading
+  tempSensor.begin();
+  tempSensor.getAddress(tempDeviceAddress, 0);
+  tempSensor.setResolution(tempDeviceAddress, resolution);
+  tempSensor.setWaitForConversion(false);
+  tempSensor.requestTemperatures();
+  delayInMillis = 750 / (1 << (12 - resolution));
+  lastTempRequest = millis();
 }//setupSensors
+
+void lightSetup() {
+  Adafruit_NeoPixel eggLight = Adafruit_NeoPixel(12, LIGHT_PIN, NEO_RGBW + NEO_KHZ800);
+  eggLight.begin();
+  //set each pixel to half brightness
+  for(int n=0;n<12;n++) {
+  eggLight.setPixelColor(n, 0, 0, 0, 50);
+  }
+  eggLight.show();
+}//lightSetup
+
 void setup()
 {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
-    ;
-  }
+  while (!Serial) {}
+  pinMode(BUTTON_PIN,INPUT);
+  pinMode(ERROR_LED_PIN, OUTPUT);
+  digitalWrite(ERROR_LED_PIN, HIGH);
   Serial.print(F("LSM check"));
     if(!lsm.begin())
   {
@@ -58,9 +111,7 @@ void setup()
   }
   setupSensors();
   Serial.println(F("Found LSM9DS0 9DOF"));
-
   Serial.println(F("Initializing SD card..."));
-
   // initalize SDFAT with speed dependent on if on breadboard, as Full speed
   // on breadboard or long wires can cause corruption in data
   if (!ON_BREADBOARD) {
@@ -73,12 +124,22 @@ void setup()
   #if DATAHEADER
     printHead();
   #endif //DATAHEADER true
-
+  lightSetup();
+  digitalWrite(ERROR_LED_PIN, LOW);
+  while(!buttonState==HIGH) {
+    //Wait and check button
+    buttonState = digitalRead(BUTTON_PIN);
+  }
+  digitalWrite(ERROR_LED_PIN, HIGH);
   #if START_ON_LAUNCH
   Serial.println(F("Waiting for launch"));
   sensors_event_t accel, mag, gyro, temp;
   while((accel.acceleration.z-9.81)<0) lsm.getEvent(&accel, &mag, &gyro, &temp);
+  #else
+  Serial.println(F("Timed launch"));
+  delay(MSTime);
   #endif
+  digitalWrite(ERROR_LED_PIN, LOW);
 
 }//setup
 
@@ -112,6 +173,7 @@ void collectData() {
    logFile.print((double) millis()/1000);
    Serial.println(millis());
    writeComma();
+
    sensors_event_t accel, mag, gyro, temp;
    lsm.getEvent(&accel, &mag, &gyro, &temp);
    logFile.print((double)accel.acceleration.x);
@@ -132,7 +194,11 @@ void collectData() {
    writeComma();
    logFile.print((double)gyro.gyro.z);
    writeComma();
-   logFile.print((double)temp.temperature);
+   if (millis() - lastTempRequest >= delayInMillis) {
+      logFile.print((double) tempSensor.getTempCByIndex(0));
+      tempSensor.setResolution(tempDeviceAddress, resolution);
+      tempSensor.requestTemperatures();
+    }
    logFile.print("\n");//new row
 }//collectData
 
